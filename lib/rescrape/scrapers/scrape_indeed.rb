fd442cf54528b69c32a4e1f95fae42ab991f89e6
Nick Bair <nickbair@golfingbuddha.com>
2017-05-23 21:26:01 -0400

    Refactor scraper, closes #33, closes #34, closes #35.

--- lib/rescrape/scrapers/scrape_indeed.rb
+++ lib/rescrape/scrapers/scrape_indeed.rb
@@ -16,50 +16,51 @@ class Rescrape::ScrapeIndeed < Rescrape::Scrape
     jobs = doc.css("#resultsCol").css(".row")
 
     jobs.each do |job|
-      # TODO:
-      # Check if job exists first!
-
       details = {}
-      details[:title] = job.css(".jobtitle").text.strip
-      details[:description] = job.css(".summary").text.strip
-
-      location = job.css(".location").text.strip.split(", ")
-      details[:city] = location[0]
-      details[:state] = location[1]
-      details[:company] = Rescrape::Company.find_or_create_by(name: job.css(".company").text.strip, city: location[0], state: location[1])
-
-      # TODO:
-      # Only make API call if company doesn't already have lat/lng
-      begin
-        data = Rescrape::Placer.new({name: details[:company].name, city: location[0], state: location[1]}).find_place
-        details[:company].lat = data["lat"]
-        details[:company].lng = data["lng"]
-      rescue
-        nil
-      end
-
-      # TODO:
-      # Only make API call if company has lat/lng
-      begin
-        map_data = Rescrape::Mapper.new(details[:company].lat, details[:company].lng).get_distance
-
-        # TODO: return value of get_distance should handle this
-        details[:distance] = map_data["distance"]["text"].to_f
-        details[:duration] = map_data["duration"]["text"]
-        details[:duration_in_traffic] = map_data["duration_in_traffic"]["text"]
-      rescue
-        nil
-      end
 
       begin
         title_div = job.css(".jobtitle").css("a")
         url = title_div[0]["href"]
         details[:url] = "https://www.indeed.com#{url}"
       rescue
-        details[:url] = ""
+        details[:url] = nil
       end
-      Rescrape::Job.find_or_create_by(details)
-      @job_counter +=1
+
+      if !Rescrape::Job.find_by(url: details[:url])
+        details[:title] = job.css(".jobtitle").text.strip
+        details[:description] = job.css(".summary").text.strip
+
+        location = job.css(".location").text.strip.split(", ")
+        details[:city] = location[0]
+        details[:state] = location[1]
+        details[:company] = Rescrape::Company.find_or_create_by(name: job.css(".company").text.strip, city: location[0], state: location[1])
+
+        if !details[:company].lat && !details[:company].lng
+          begin
+            data = Rescrape::Placer.new({name: details[:company].name, city: location[0], state: location[1]}).find_place
+            details[:company].lat = data["lat"]
+            details[:company].lng = data["lng"]
+          rescue
+            nil
+          end
+        end
+
+        if details[:company].lat && details[:company].lng
+          begin
+            map_data = Rescrape::Mapper.new(details[:company].lat, details[:company].lng).get_distance
+
+            # TODO: return value of get_distance should handle this
+            details[:distance] = map_data["distance"]["text"].to_f
+            details[:duration] = map_data["duration"]["text"]
+            details[:duration_in_traffic] = map_data["duration_in_traffic"]["text"]
+          rescue
+            nil
+          end
+        end
+
+        Rescrape::Job.create(details)
+        @job_counter +=1
+      end
     end
 
     pages = doc.css(".pagination").css("a")
